797f9fef33ff92af7f2e9ea677681749dbe19dbb
manodavi <Davitpontas@gmail.com>
2024-10-14 16:10:15 -0300

    text
    hooks
    
    end
    
    end

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import './styles.css';
+
+export class Button extends Component{
+    render(){
+        const {text ,action, disabled} = this.props;
+        return(
+            <button
+                disabled={disabled}
+                className='button'
+                onClick={action}>
+                    {text}
+                </button>
+        )
+    }
+}
---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+export const TextInput = ({ searchValue, handleChange }) =>  (
+    <input
+      className="text-input"
+      onChange={handleChange}
+      value={searchValue}
+      type="search"
+      placeholder="Type your search"
+    />
+  );
